f9f2a0316f4325c1f3402aa1fa35cfd335162e70
kogupta <kohinoor.gupta@gmail.com>
2024-11-28 03:08:03 +0530

    duck l33t code

--- src/doofus.rs
+++ src/doofus.rs
@@ -40,6 +40,44 @@ fn flood_fill(image: Vec<Vec<i32>>, sr: i32, sc: i32, color: i32) -> Vec<Vec<i32
     image
 }
 
+fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+    fn dfs(grid: &Vec<Vec<char>>, row: i32, col: i32, visited: &mut [Vec<bool>]) {
+        let m = grid.len() as i32;
+        let n = grid[0].len() as i32;
+        if row < 0 || row >= m || col < 0 || col >= n {
+            return;
+        }
+
+        let row = row as usize;
+        let col = col as usize;
+        if visited[row][col] || grid[row][col] == '0' {
+            return;
+        }
+
+        visited[row][col] = true;
+
+        let row = row as i32;
+        let col = col as i32;
+        dfs(grid, row - 1, col, visited);
+        dfs(grid, row + 1, col, visited);
+        dfs(grid, row, col - 1, visited);
+        dfs(grid, row, col + 1, visited);
+    }
+
+    let mut id: i32 = 0;
+    let mut visited = vec![vec![false; grid[0].len()]; grid.len()];
+    for (i, chars) in grid.iter().enumerate() {
+        for (j, c) in chars.iter().enumerate() {
+            if *c == '1' && !visited[i][j] {
+                id += 1;
+                dfs(&grid, i as i32, j as i32, &mut visited);
+            }
+        }
+    }
+
+    id
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
